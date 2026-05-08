5741a37c533d03bbe5ff50e12f41f3711e44cea9
devpark435 <devpark435@gmail.com>
2026-05-09 01:21:36 +0900

    feat(analysis): AnalysisData에 failure/confidence 필드 추가

--- lib/features/analysis/data/services/video_analysis_service.dart
+++ lib/features/analysis/data/services/video_analysis_service.dart
@@ -1,3 +1,5 @@
+import 'package:bowling_diary/features/analysis/domain/entities/rpm_result.dart';
+import 'package:bowling_diary/features/analysis/domain/entities/speed_result.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/foundation.dart';
 import 'package:image/image.dart' as img;
@@ -7,12 +9,20 @@ class AnalysisData {
   final int? rpmEstimated;
   final int framesAnalyzed;
   final int fpsUsed;
+  final SpeedFailure? speedFailure;
+  final RpmFailure? rpmFailure;
+  final double speedConfidence;
+  final double rpmConfidence;
 
   const AnalysisData({
     this.speedKmh,
     this.rpmEstimated,
     required this.framesAnalyzed,
     required this.fpsUsed,
+    this.speedFailure,
+    this.rpmFailure,
+    this.speedConfidence = 0.0,
+    this.rpmConfidence = 0.0,
   });
 }
 
